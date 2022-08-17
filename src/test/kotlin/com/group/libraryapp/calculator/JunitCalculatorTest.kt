dcd3a6bbb5e83af90974ef5e783bd1bc014d0345
seojeongkuk <wjdrnr6662@gmail.com>
2022-08-17 17:13:04 +0900

    Junit5를 이용한 계산기 테스트

---
+++ src/test/kotlin/com/group/libraryapp/calculator/JunitCalculatorTest.kt
@@ -0,0 +1,67 @@
+package com.group.libraryapp.calculator
+
+import org.assertj.core.api.AssertionsForInterfaceTypes.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class JunitCalculatorTest {
+    @Test
+    fun addTest() {
+        // given
+        val calculator = Calculator(5)
+
+        // when
+        calculator.add(3)
+
+        // then
+        assertThat(calculator.number).isEqualTo(8)
+    }
+
+    @Test
+    fun minusTest() {
+        // given
+        val calculator = Calculator(5)
+
+        // when
+        calculator.minus(3)
+
+        // then
+        assertThat(calculator.number).isEqualTo(2)
+    }
+
+    @Test
+    fun multiplyTest() {
+        // given
+        val calculator = Calculator(5)
+
+        // when
+        calculator.multiply(3)
+
+        // then
+        assertThat(calculator.number).isEqualTo(15)
+    }
+
+    @Test
+    fun divideTest() {
+        // given
+        val calculator = Calculator(5)
+
+        // when
+        calculator.divide(2)
+
+        // then
+        assertThat(calculator.number).isEqualTo(2)
+    }
+
+    @Test
+    fun divideExceptionTest() {
+        // given
+        val calculator = Calculator(5)
+
+        // when
+
+
+        // then
+        assertThrows<java.lang.IllegalArgumentException> { calculator.divide(0) }
+    }
+}
